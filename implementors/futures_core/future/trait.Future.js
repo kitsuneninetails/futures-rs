b34730dc6e6927a92251c9e809e99233d1fdef84
Travis CI User <travis@example.org>
2018-07-09 22:20:28 +0000

    Update documentation

---
+++ implementors/futures_core/future/trait.Future.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["futures_core"] = [{text:"impl&lt;F:&nbsp;<a class=\"trait\" href=\"futures_core/future/trait.Future.html\" title=\"trait futures_core::future::Future\">Future</a>&gt; <a class=\"trait\" href=\"futures_core/future/trait.Future.html\" title=\"trait futures_core::future::Future\">Future</a> for <a class=\"struct\" href=\"futures_core/future/struct.FutureOption.html\" title=\"struct futures_core::future::FutureOption\">FutureOption</a>&lt;F&gt;",synthetic:false,types:["futures_core::future::option::FutureOption"]},{text:"impl&lt;T&gt; <a class=\"trait\" href=\"futures_core/future/trait.Future.html\" title=\"trait futures_core::future::Future\">Future</a> for <a class=\"struct\" href=\"futures_core/future/struct.ReadyFuture.html\" title=\"struct futures_core::future::ReadyFuture\">ReadyFuture</a>&lt;T&gt;",synthetic:false,types:["futures_core::future::ReadyFuture"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
